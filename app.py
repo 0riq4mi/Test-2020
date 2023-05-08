97aa7eca1a1e39224dd79dfa73e5283d575d37d6
Oktay Çaylak <0riq4mi@gmail.com>
2023-05-08 14:30:23 +0300

    Costimezed

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',answer = "evet")
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
