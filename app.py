560aa63c1fdb02be630cd602f978dedca67e568e
Oktay Çaylak <0riq4mi@gmail.com>
2023-05-09 09:49:45 +0300

    Added Register

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ from flask_mysqldb import MySQL
 from wtforms import Form,StringField,TextAreaField,PasswordField,validators
 from passlib.hash import sha256_crypt
 
-
-
+#! Kullanıcı Kayıt Formu
+class RegisterForm(Form):
+    name = StringField("İsim Soyisim", validators=[validators.Length(min=4,max=25)])
+    username = StringField("Kullanıcı Adı", validators=[validators.Length(min=5,max=35)])
+    email = StringField("Email Adresi", validators=[validators.Email(message="Lütfen Geçerli bir email adresi giriniz")])
+    password = PasswordField("Paralo", validators=[validators.DataRequired(message="Lütfen bir paralo giriniz"),
+                                                   validators.EqualTo(fieldname="confirm",message="Parolanız uyuşmuyor.")])
+    confirm = PasswordField("Parola Doğrula")
 app = Flask(__name__)
 
 app.config["MYSQL_HOST"] = "localhost"
@@ -23,5 +29,16 @@ def index():
 def about():
     return render_template('about.html')
 
+#! REGISTER TAB-------------------------------------------------
+@app.route('/register',methods=['GET','POST'])
+def register():
+    form = RegisterForm(request.form)
+    
+    if request.method == 'POST':
+        return redirect(url_for("index"))
+    else:
+        return render_template("register.html",form=form)
+
 if __name__ == '__main__':
     app.run(debug=True)
+
